6df463129b013e12af8a08c897a7db66b0eb842b
m01x <fortiz.informatica@gmail.com>
2024-09-03 22:07:59 -0400

    feat(Styles, Categories): Se agregan estilos y se comienza a trabajar los primeros estados

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(['One Punch', 'Makima']);
+  console.log(categories);
   return (
     <>
+      {/* Titulo */}
       <h1>GifExpertApp!</h1>
+
+      {/* Input */}
+      {/* Listado de items (Gifs) */}
+      <ol>
+        {categories.map(category => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
+      {/* Gif Items */}
     </>
   );
 };
